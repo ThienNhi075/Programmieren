3e8e6b9055ee18b8a766aaeeb4853a4dbc30bc92
ThienNhi075 <96128557+ThienNhi075@users.noreply.github.com>
2021-12-14 16:03:01 +0100

    File downloaded

---
+++ Bmi.py
@@ -0,0 +1,3 @@
+x = float(input("Gebe deine Grösse in m ein: ")) # Grösse eingegeben
+y = float(input("Gebe dein Gewicht in kg ein: ")) # Gewicht eingeben
+print (("dein BMI ist: ") + str(y / (x * x))) # Gewicht wird durch das Quadrat von der Grösse geteilt
